423d428a0f644b01a12f34ee61cb3d6c7beb9254
Joao Carvalho <fonzie3333@gmail.com>
2023-01-30 18:41:51 +0000

    Add documentation to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# @model
+#
+# @property id(required) [integer] the identifier for the article
+# @property external_id [integer] the external indentifier for the article. It must be unique
+# @property liked [boolean] the boolean representing if the article is liked
+#
 class Article < ApplicationRecord
   validates :external_id, uniqueness: true
 end
