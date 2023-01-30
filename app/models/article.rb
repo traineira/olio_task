f6e9ba321309585c8898eaecd665238b48ee3bd2
Joao Carvalho <fonzie3333@gmail.com>
2023-01-30 17:07:52 +0000

    Add uniqueness validation for Article external_id attribute

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  validates :external_id, uniqueness: true
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_uniqueness_of(:external_id) }
 end
